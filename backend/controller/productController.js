31de75bf3d16916c4e1fc05e22aa73b35c18faeb
Mayankesh Jha <77605686+mayankesh239@users.noreply.github.com>
2021-12-06 17:34:14 +0530

    Initial Commit

---
+++ backend/controller/productController.js
@@ -0,0 +1,7 @@
+// Get All Product
+exports.getAllProducts = (req, res) => {
+
+    res.status(200).json({
+        message:'Our route is working fine!!!!!! :-) <3 XD',
+    });
+};
---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const app = require("./app");
+
+const dotenv = require('dotenv');
+
+dotenv.config({ path: "backend/config/config.env" });
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is working on http://localhost:${process.env.PORT}`);
+});
