b214793a1e2658176b2348801f9d6933d3a8edaa
Mayankesh Jha <77605686+mayankesh239@users.noreply.github.com>
2021-12-11 11:42:00 +0530

    Initial Commit

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const app = express();
-
+const cookieParser = require('cookie-parser');
+const middleware = require('./middleware/error');
 app.use(express.json());
+app.use(cookieParser());
 
 // Route Imports
+
+//Product Route
 const product = require("./routes/productRoute");
 
-app.use("/api/v1", product);
+//User Route
+const user = require("./routes/userRoutes");
 
+//Order Route
+const order= require("./routes/orderRoute");
+
+app.use("/api/v1", product);
+app.use("/api/v1", user);
+app.use("/api/v1",order );
+
+//middleware for errors 
+app.use(middleware);
 
 module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,35 @@
 const app = require("./app");
 
 const dotenv = require('dotenv');
+const connectDatabase =require('./config/database');
 
+// Handling Uncaught Exception
+process.on('uncaughtException',(err)=>{
+    console.log(`Error: ${err.message}`);
+    console.log("Shutting down the server due to Uncaught exception");
+
+    server.close(()=>{
+        process.exit(1);
+    });
+})
+
+//config
 dotenv.config({ path: "backend/config/config.env" });
 
+//connecting to database
+connectDatabase();
 
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
     console.log(`Server is working on http://localhost:${process.env.PORT}`);
+});
+
+
+//Unhandled promise rejection
+process.on('unhandledRejection',(err)=>{
+    console.log(`Error: ${err.message}`);
+    console.log("Shutting down the server due to unhandled promise rejection");
+
+    server.close(()=>{
+        process.exit(1);
+    });
 });
