d6b9f624e3f429c9a50c4618efba5719503eecab
mayankesh239 <mayankesh.ss@gmail.com>
2021-12-27 23:04:16 +0530

    updated for production --try

--- backend/app.js
+++ backend/app.js
@@ -4,11 +4,12 @@ const cookieParser = require('cookie-parser');
 const middleware = require('./middleware/error');
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
-const dotenv = require('dotenv');
 
 
 //config
-dotenv.config({ path: "./config/config.env" });
+if (process.env.NODE_ENV !== "PRODUCTION") {
+    require("dotenv").config({ path: "backend/config/config.env" });
+}
 
 
 app.use(express.json());
@@ -25,15 +26,22 @@ const product = require("./routes/productRoute");
 const user = require("./routes/userRoutes");
 
 //Order Route
-const order= require("./routes/orderRoute");
+const order = require("./routes/orderRoute");
 
 //Payment Route
-const payment= require("./routes/paymentRoute");
+const payment = require("./routes/paymentRoute");
 
 app.use("/api/v1", product);
 app.use("/api/v1", user);
-app.use("/api/v1",order );
-app.use("/api/v1",payment );
+app.use("/api/v1", order);
+app.use("/api/v1", payment);
+
+
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+});
 
 //middleware for errors 
 app.use(middleware);
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 const app = require("./app");
 
-const dotenv = require('dotenv');
 const connectDatabase = require('./config/database');
 const cloudinary = require("cloudinary");
 
@@ -14,8 +13,11 @@ process.on('uncaughtException', (err) => {
     });
 })
 
+
 //config
-dotenv.config({ path: "backend/config/config.env" });
+if (process.env.NODE_ENV !== "PRODUCTION") {
+    require("dotenv").config({ path: "backend/config/config.env" });
+}
 
 //connecting to database
 connectDatabase();
