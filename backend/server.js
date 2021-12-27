7b59202fd61114233b0edd3a404b39cd9e4cc6a2
mayankesh239 <mayankesh.ss@gmail.com>
2021-12-28 01:41:53 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -28,8 +28,10 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const server = app.listen(process.env.PORT, () => {
-    console.log(`Server is working on http://localhost:${process.env.PORT}`);
+var port = process.env.PORT || 4000;
+
+const server = app.listen(port, () => {
+    console.log(`Server is working on http://localhost:${port}`);
 });
 
 
